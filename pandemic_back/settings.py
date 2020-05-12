581d7d50171a3fc232678d0bf55913ee6cc02f47
Nicolas MATET <nicolas.matet@gmail.com>
2020-05-12 12:20:18 +0200

    statics

--- pandemic_back/settings.py
+++ pandemic_back/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -95,12 +97,19 @@ WSGI_APPLICATION = 'pandemic_back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DB_HOST = os.environ.get('DATABASE_URL:', 'localhost')
+DB_PORT = os.environ.get('DB_PORT', 3306)
+DB_NAME = os.environ.get('DB_NAME', 'preprod')
+DB_USER = os.environ.get('DB_USER', 'root')
+DB_PSWD = os.environ.get('DB_PSWD', 'rootAsse42!')
+
 DATABASES = {
-    'default': {
+    'dev': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
